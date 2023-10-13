0fccf118b94a11f72d68f93ea169cee068d4cbef
Patrick <patrick@Mac-mini.local>
2023-10-13 10:38:56 +0200

    simplified logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,18 @@ const App = () => {
   const review = reviews[index];
   const length = reviews.length;
 
-  const changeIndex = (newIndex) => {
-    newIndex = newIndex < 0 ? length - 1 : newIndex;
-    setIndex(newIndex % length);
-    // if (newIndex >= 0 && newIndex <= reviews.length - 1) {
-    //   setIndex(newIndex);
-    // }
-    // else if (newIndex === 'random') {
-    //   const random = Math.floor(Math.random() * (reviews.length - 1));
-    //   setIndex(random);
-    // }
+  const changeIndex = (change) => {
+    setIndex((currentIndex) => {
+      while (change === 0) {
+        change = Math.floor(Math.random() * length);
+      }
+      const newIndex = currentIndex + change < 0 ? length - 1 : currentIndex + change;
+      return newIndex % length;
+    })
   }
 
   return <main className="main">
-      <Review key={review.id} review={{...review, index, length, changeIndex} }/>
+      <Review review={{...review, changeIndex} }/>
     </main>;
 };
 export default App;
--- src/Review.jsx
+++ src/Review.jsx
@@ -1,30 +1,28 @@
 import { FaQuoteRight, FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 const Review = ({review}) => {
-    const { id, name, job, image, text, index, length, changeIndex } = review;
+    const { name, job, image, text, changeIndex } = review;
 
   return (
-    <div className="review">
+    <article className="review">
         <div className="img-container">
             <img className="person-img" src={image} alt={name} />
-            <div className="quote-icon">
+            <span className="quote-icon">
                 <FaQuoteRight />
-            </div>
+            </span>
         </div>
-        <div className="author">
-            {name}
-        </div>
-        <div className="job">{job}</div>
-        <div className="info">{text}</div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
         <div className='btn-container'>
-            <button className='prev-btn' onClick={() => changeIndex(index - 1)} ><FaArrowLeft /></button>
-            <button className='prev-btn' onClick={() => changeIndex(index + 1)}><FaArrowRight /></button>
+            <button className='prev-btn' onClick={() => changeIndex(-1)} ><FaArrowLeft /></button>
+            <button className='next-btn' onClick={() => changeIndex(1)}><FaArrowRight /></button>
         </div>
         <div>
-            <button className='btn btn-hipster' onClick={() => changeIndex(Math.floor(Math.random() * (length - 1)))}>Surprise me!</button>
+            <button className='btn btn-hipster' onClick={() => changeIndex(0)}>Surprise me!</button>
         </div>
         
-    </div>
+    </article>
   )
 }
 export default Review
